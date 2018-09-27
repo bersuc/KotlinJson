342c4cf97417092f56d9c1473e5cd71d10ddc763
Bersuc <dbersuc@gmail.com>
2018-09-27 16:36:14 -0300

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,46 @@
+import org.json.JSONArray
+import org.json.JSONObject
+import org.json.simple.parser.JSONParser
+import java.net.URL
+
+fun main(args: Array<String>) {
+
+    //Define JSON URL to parse
+    val enderecoJson = ""
+
+    //Call parser and identify first Key
+    val parser = JSONParser()
+    var obj = parser.parse(URL(enderecoJson).readText())
+    var jsonObject = JSONObject(obj as MutableMap<Any?, Any?>?)
+    val placemarks = jsonObject.getJSONArray("placemarks")
+
+
+    // Loop at all cars in JSON
+    for (i in 0 until placemarks.length()) {
+        val rec = placemarks.getJSONObject(i)
+        var endereco = rec.getString("address")
+        var coordinates = rec.getJSONArray("coordinates")
+        var motor = rec.getString("engineType")
+        var exteriorState = rec.getString("exterior")
+        var fuel = rec.getInt("fuel")
+        var interiorState = rec.getString("interior")
+        var nome = rec.getString("name")
+        var vin = rec.getString("vin")
+
+        // Good Looking printing
+        println("""
+            ########################
+            Car name: $nome
+            Located at: $coordinates
+            Type of Engine: $motor
+            Fuel type: $fuel
+            Exterior State: $exteriorState
+            Interior State: $interiorState
+            vin number: $vin
+            #########################
+        """.trimIndent())
+    }
+
+    println("\n${placemarks.length()} lines read")
+
+}
